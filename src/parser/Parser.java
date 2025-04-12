c87ff168526a06b51782d648d7a2f853fe12269a
BetelGeuseee <shirshakupadhayay182@gmail.com>
2025-04-12 13:21:43 +0545

    research on working of CFG and removing ambiguity

--- src/parser/Parser.java
+++ src/parser/Parser.java
@@ -4,10 +4,24 @@ import model.Token;
 
 import java.util.List;
 
-public class Parser {
-    private List<Token> tokens;
+/***
+ * final grammar
+ * -----------------------
+ * expression → equality ;
+ * equality→ comparison ( ( "!=" | "==" ) comparison )* ;
+ * comparison→ term ( ( ">" | ">=" | "<" | "<=" ) term )* ;
+ * term→ factor ( ( "-" | "+" ) factor )* ;
+ * factor→ unary ( ( "/" | "*" ) unary )* ;
+ * unary→ ( "!" | "-" ) unary
+ * primary→ NUMBER | STRING | "true" | "false" | "nil"| primary | "(" expression ")" ;
+ */
 
+public class Parser {
+    private final List<Token> tokens;
+    private int current = 0;
     public Parser(List<Token> tokens){
         this.tokens = tokens;
     }
+
+
 }
